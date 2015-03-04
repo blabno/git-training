2a962a4eb26212831f5aada0eafcf92a76728f31
student <student@lab112e1.pwsztar.edu.pl>
2015-03-04 12:53:44 +0100

    substract

--- calculator.js
+++ calculator.js
@@ -5,3 +5,6 @@ function Calculator() {
 Calculator.prototype.add = function(a, b) {
 	return a+b;
 }
+Calculator.prototype.substract = function(a, b) {
+    return a-b;
+}
--- calculator.test.js
+++ calculator.test.js
@@ -4,4 +4,9 @@ describe('Calculator', function () {
             expect(new Calculator().add(1, 2)).toEqual(3);
         });
     });
+    describe('substract', function () {
+        it('should substract two numbers', function () {
+            expect(new Calculator().add(2, 1)).toEqual(1);
+        });
+    });
 });
